3085f1a0cb81b644e66c269e5d744ac119376e1c
Oliver De Sa <oliver.desa@mail.utoronto.ca>
2022-12-01 21:16:22 -0500

    added a function to read file and troubleshot it

--- myutils.R
+++ myutils.R
@@ -2,3 +2,17 @@
 # SciNet username: tmp_odesa
 # Description:
 #   Functions to be used for Assignment 10
+
+word_count <- function(words, stop_words=NULL){
+  if (is.null(stop_words)){
+    words <- strsplit(words, "[^a-zA-Z']+")
+    wordDF <- data.frame(table(wordList[[1]]))
+    colnames(wordDF) <- c('word', 'count')
+  } else {
+    words <- strsplit(words, "[^a-zA-Z']+")
+    wordDF <- data.frame(table(wordList[[1]]))
+    colnames(wordDF) <- c('word', 'count')
+    index <- wordDF$word %in% stop_words
+    wordDF <- wordDF[!index,]
+  }
+}
